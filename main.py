6ead43d09700e5f314f0b298647f4b5f0c5c88ed
jpmg411 <jpmg411@gmail.com>
2020-04-03 13:18:54 +0100

    Minimum distance classifier

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
+#pip install liac-arff
 import arff
 from os.path import join
 from os import listdir
@@ -13,9 +14,10 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import euclidean_distances
 
 
-# In[2]:
+# In[23]:
 
 
 path = join('wisdm-dataset', 'arff_files')
@@ -25,6 +27,7 @@ sensors = ['accel', 'gyro']
 
 def arff_to_df(cur_path):
     dt = arff.load(open(cur_path))
+    print(type(dt))
     return pd.DataFrame(dt['data'], columns=[row[0] for row in dt['attributes']])
 
 
@@ -34,7 +37,6 @@ def fix_arff_files():
             cur_path = join(path, d, s)
             files = [f for f in listdir(cur_path) if f.endswith('.arff')]
             for f in files:
-                data = ''
                 with open(join(cur_path, f), 'r+') as file:
                     data = file.read()
                     data.replace('{ ', '{')
@@ -54,22 +56,30 @@ def read_all_files():
     return df
 
 
-# In[3]:
+# In[24]:
 
 
 df = read_all_files()
 
 
-# In[4]:
+# In[26]:
 
 
-def standardize_df(df):
+cur_device = devices[0]
+cur_sensor = sensors[0]
+
+def drop_non_features(df, device, sensor):
+    return df[cur_device,cur_sensor].drop(['ACTIVITY', 'class'], axis=1)
+
+cur_df = drop_non_features(df, cur_device, cur_sensor)
+
+def normalize_df(df):
     return (df-df.mean())/df.std()
 
-cur_df = standardize_df(df['phone','accel'].iloc[:,1:-1])
+cur_df = normalize_df(cur_df)
 
 
-# In[5]:
+# In[27]:
 
 
 def drop_correlated(df, min_corr=0.95):
@@ -85,12 +95,43 @@ def drop_correlated(df, min_corr=0.95):
 cur_df = drop_correlated(cur_df)
 
 
+# In[28]:
+
+
+#Calculate class prototypes
+temp = df[cur_device, cur_sensor]
+activities = temp['ACTIVITY'].unique()
+class_means = [cur_df[temp['ACTIVITY']==act].mean().values for act in activities]
+
+
+# In[29]:
+
+
+n_rows = cur_df.shape[0]
+correct_classified = 0
+# Minimum distance classifier
+for i in range(n_rows):
+    cur_sample = [cur_df.iloc[i]]
+    min_dist_ind = np.argmin(euclidean_distances(cur_sample, class_means))
+    if activities[min_dist_ind] == temp['ACTIVITY'].iloc[i]:
+        correct_classified += 1
+print('Correct:', correct_classified)
+print('Wrong:', n_rows-correct_classified)
+print('Percent', correct_classified/n_rows)
+
+
 # In[6]:
 
 
 model = PCA(.99) # Keep n components that fit 99% of data
 model.fit(cur_df)
 print(model.n_components_)
+
+
+# In[10]:
+
+
+
 
 
 # In[ ]:
